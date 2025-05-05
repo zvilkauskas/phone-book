3b346d9628b231a72335cd31f437c1f5629b6ab6
Zilvinas <zilvinas.vilkauskas@gmail.com>
2025-05-05 21:53:47 +0300

    Add routes for viewing, creating, and editing contacts (protected by auth)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\Contacts\ContactCreate;
+use App\Livewire\Contacts\ContactEdit;
+use App\Livewire\Contacts\ContactIndex;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -11,5 +14,11 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/contacts', ContactIndex::class)->name('contact.index');
+    Route::get('/contacts/create', ContactCreate::class)->name('contact.create');
+    Route::get('contacts/{contact}/edit', ContactEdit::class)->name('contact.edit');
+});
 
 require __DIR__.'/auth.php';
