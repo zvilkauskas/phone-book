6d74ad7147294b91ed1971ee163ac89cd95b80ae
Zilvinas <zilvinas.vilkauskas@gmail.com>
2025-05-06 15:53:01 +0300

    Add model factories for Contact and Country
    Introduce factories for Contact and Country models to streamline test data generation. Modified models to use the HasFactory trait, enabling factory support. This improves testing and seeding capabilities.

---
+++ database/factories/CountryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Country>
+ */
+class CountryFactory extends Factory
+{
+    protected $model = Country::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->country(),
+            'iso_code' => fake()->countryCode(),
+            'country_code' => fake()->randomNumber(3),
+        ];
+    }
+}
